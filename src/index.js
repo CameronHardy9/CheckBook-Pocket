bf1ad07caf22d36093921e93284f8da34e69b076
Cameron Hardy <camhardy99@gmail.com>
2022-01-06 11:32:16 -0600

    Added nested route structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route} from 'react-router-dom';
+import Purchases from './components/Purchases';
+import Item from './components/Item';
+import Budget from './components/Budget';
+import Home from './components/Home';
+import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<h1>No User</h1>} />
+                <Route path=":id" element={<App />}>
+                    <Route path="home" element={<Home />} />
+                    <Route path="budget" element={<Budget />}/>
+                    <Route path="purchases" element={<Purchases />}>
+                        <Route path=":itemId" element={<Item />} />
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
