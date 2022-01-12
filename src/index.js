0573f03ef89bedc5a4333914c7feaa4fbd58fd01
Cameron Hardy <camhardy99@gmail.com>
2022-01-12 10:23:24 -0600

    Moved some routes to child component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Routes, Route} from 'react-router-dom';
-import Purchases from './components/Purchases';
-import Item from './components/Item';
-import Budget from './components/Budget';
-import Home from './components/Home';
 import App from './App';
 
 ReactDOM.render(
@@ -15,13 +11,8 @@ ReactDOM.render(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<h1>No User</h1>} />
-                <Route path=":id" element={<App />}>
-                    <Route path="home" element={<Home />} />
-                    <Route path="budget" element={<Budget />}/>
-                    <Route path="purchases" element={<Purchases />}>
-                        <Route path=":itemId" element={<Item />} />
-                    </Route>
-                </Route>
+                <Route path=":id/*" element={<App />} />
+                <Route path="/404" element={<h1>404: Page not found.</h1>} />
             </Routes>
         </BrowserRouter>
     </React.StrictMode>,
