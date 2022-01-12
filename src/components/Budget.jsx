4a41b29ca93dc2e2253ebe1b1e8900f51fbae292
Cameron Hardy <camhardy99@gmail.com>
2022-01-12 10:29:58 -0600

    Added new MUI components and basic functionality

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,6 +1,37 @@
-function Budget() {
+import {useNavigate} from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import {useState} from 'react';
+
+function Budget(props) {
+    const navigate = useNavigate();
+    const [value, setValue] = useState(0);
+    const [currBud, setCurrBud] = useState(props.currBud);
+    const [setBud, setSetBud] = useState(props.setBud);
+
     return(
-        <h1>Budget</h1>
+        <>
+            <h2>${setBud}</h2>
+            <h2>${currBud}</h2>
+            <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+                <Box sx={{ width: "auto"}}>
+                    <BottomNavigation
+                        showLabels
+                        value={value}
+                        onChange={(event, newValue) => {
+                        setValue(newValue);
+                        }}
+                    >
+                        <BottomNavigationAction label="Set Budget" icon={<AttachMoneyIcon />} onClick={() => console.log("Set Budget")} />
+                        <BottomNavigationAction label="Back" icon={<ArrowBackIcon />} onClick={() => navigate('../')} />
+                    </BottomNavigation>
+                </Box>
+            </Paper>
+        </>
     )
 }
 
