32638d9a7201ab8e43e77e2602a2c569025b67c0
Cameron Hardy <camhardy99@gmail.com>
2022-01-12 11:12:50 -0600

    Removed old import. Styled progress visual

--- src/components/BudgetVisual.jsx
+++ src/components/BudgetVisual.jsx
@@ -1,29 +1,30 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import CircularProgress from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 export default function BudgetVisual(props) {
   return (
-    <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-      <CircularProgress variant="determinate" value={props.value} />
-      <Box
-        sx={{
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
-          position: 'absolute',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Typography variant="caption" component="div" color="text.secondary">
-          {`${Math.round(props.value)}%`}
-        </Typography>
-      </Box>
-    </Box>
+    <div className="visualContainer" style={{display: "flex", width: "100%", height: "75%", alignItems: "center", justifyContent: "center"}}>
+        <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+          <CircularProgress size={300} variant="determinate" value={props.value} />
+          <Box
+            sx={{
+              top: 0,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              position: 'absolute',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Typography variant="caption" sx={{fontSize: "100px"}} component="div" color="text.secondary">
+              {`${Math.round(props.value)}%`}
+            </Typography>
+          </Box>
+        </Box>
+    </div>
   );
 };
