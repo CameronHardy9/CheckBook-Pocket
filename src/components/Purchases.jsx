c0f347cd1273c31040d3b3108a6f93aa1a5ad492
Cameron Hardy <camhardy99@gmail.com>
2022-01-13 15:36:56 -0600

    Added map for purchases to load on screen. Still need to make purchases into MUI portals. Removed default highlight selection on menu

--- src/components/Purchases.jsx
+++ src/components/Purchases.jsx
@@ -1,6 +1,7 @@
 import {useNavigate, Outlet} from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
@@ -9,27 +10,32 @@ import {useState} from 'react';
 
 function Purchases(props) {
     const navigate = useNavigate();
-    const [value, setValue] = useState(0);
 
     return(
-        <>
-            <button onClick={() => navigate("1234")}>item</button>
+        <div className='viewWindow' style={{marginTop: "55px"}}>
+            {props.purchases.map((item) => {
+                return(
+                <Paper key={item.uniqid} sx={{width: "auto", height: "auto", padding: "20px", margin: "10px"}} elevation={3}>
+                    <Stack direction="row" spacing={"auto"}>
+                        <span>{item.store}</span>
+                        <span>${item.amount.toFixed(2)}</span>
+                        <span>{item.date}</span>
+                    </Stack>
+                </Paper>
+                )
+            })}
             <Outlet />
             <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
                 <Box sx={{ width: "auto"}}>
                     <BottomNavigation
                         showLabels
-                        value={value}
-                        onChange={(event, newValue) => {
-                        setValue(newValue);
-                        }}
                     >
                         <BottomNavigationAction label="Add Purchase" icon={<PlaylistAddIcon />} onClick={() => console.log("Add Purchase")} />
                         <BottomNavigationAction label="Back" icon={<ArrowBackIcon />} onClick={() => navigate('../')} />
                     </BottomNavigation>
                 </Box>
             </Paper>
-        </>
+        </div>
     )
 }
 
