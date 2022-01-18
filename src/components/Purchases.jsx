f4333ba6befd12228156cffb2e9b57f697ef5200
Cameron Hardy <camhardy99@gmail.com>
2022-01-18 11:05:45 -0600

    Working to add MUI Portal for each purchase item

--- src/components/Purchases.jsx
+++ src/components/Purchases.jsx
@@ -1,22 +1,38 @@
 import {useNavigate} from 'react-router-dom';
+import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import {useState} from 'react';
+import Portal from '@mui/material/Portal';
+import {useState, useRef} from 'react';
 
 function Purchases(props) {
     const navigate = useNavigate();
+    const [show, setShow] = useState(false);
+    const container = useRef(null);
+
+    const handleClick = () => {
+        setShow(!show);
+        console.log(container)
+      };
 
     return(
-        <div className='viewWindow' style={{marginTop: "55px"}}>
+        <div style={{overflow: "scroll", flexGrow: 1}}>
             {props.purchases.map((item) => {
                 return(
-                <Paper key={item.uniqid} sx={{width: "auto", height: "auto", padding: "20px", margin: "10px"}} elevation={3}>
-                    <Stack direction="row" spacing={"auto"}>
-                        <span>{item.store}</span>
-                        <span>${item.amount.toFixed(2)}</span>
-                        <span>{item.date}</span>
-                    </Stack>
-                </Paper>
+                    <>
+                        <Paper ref={container} key={item.uniqid} onClick={handleClick} sx={{width: "auto", height: "auto", padding: "20px", margin: "10px"}} elevation={3}>
+                            <Stack direction="row" spacing={"auto"}>
+                                <span>{item.store}</span>
+                                <span>${item.amount.toFixed(2)}</span>
+                                <span>{item.date}</span>
+                            </Stack>
+                        </Paper>
+                        {show ? (
+                            <Portal container={container.current}>
+                                <span>Hello</span>
+                            </Portal>
+                        ) : null}
+                    </>
                 )
             })}
         </div>
