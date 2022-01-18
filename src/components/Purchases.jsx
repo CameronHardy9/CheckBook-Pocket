5c2fb397620ed1113b8a14b9c7ff06ca6c4170e9
Cameron Hardy <camhardy99@gmail.com>
2022-01-18 15:22:29 -0600

    Updated clickHandler. Removed MUI Portal. Added delete button

--- src/components/Purchases.jsx
+++ src/components/Purchases.jsx
@@ -1,38 +1,34 @@
-import {useNavigate} from 'react-router-dom';
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import Portal from '@mui/material/Portal';
-import {useState, useRef} from 'react';
+import Box from '@mui/material/Box';
+import DeleteIcon from '@mui/icons-material/Delete';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 function Purchases(props) {
-    const navigate = useNavigate();
-    const [show, setShow] = useState(false);
-    const container = useRef(null);
-
-    const handleClick = () => {
-        setShow(!show);
-        console.log(container)
+    const handleClick = (id) => {
+        const element = document.querySelector(`[data-key='${id}']`)
+        element.hidden = !element.hidden;
       };
 
     return(
         <div style={{overflow: "scroll", flexGrow: 1}}>
             {props.purchases.map((item) => {
                 return(
-                    <>
-                        <Paper ref={container} key={item.uniqid} onClick={handleClick} sx={{width: "auto", height: "auto", padding: "20px", margin: "10px"}} elevation={3}>
-                            <Stack direction="row" spacing={"auto"}>
+                    <div key={item.uniqid}>
+                        <Paper onClick={() => handleClick(item.uniqid)} sx={{width: "auto", height: "auto", padding: "20px", margin: "10px"}} elevation={3}>
+                            <Stack direction="row" spacing="auto">
                                 <span>{item.store}</span>
                                 <span>${item.amount.toFixed(2)}</span>
                                 <span>{item.date}</span>
                             </Stack>
                         </Paper>
-                        {show ? (
-                            <Portal container={container.current}>
-                                <span>Hello</span>
-                            </Portal>
-                        ) : null}
-                    </>
+                        <Paper sx={{width: "auto", height: "auto", padding: "10px", margin: "10px", backgroundColor: "#c6c6c6"}} variant='outlined' data-key={item.uniqid} hidden>
+                            <Stack direction="row" spacing={20} justifyContent="center">
+                                <DeleteIcon fontSize='large' sx={{color: "white"}} onClick={() => console.log("Delete")} />
+                                <HighlightOffIcon fontSize='large' sx={{color: "white"}} onClick={() => console.log("Cancel")} />
+                            </Stack>
+                        </Paper>
+                    </div>
                 )
             })}
         </div>
