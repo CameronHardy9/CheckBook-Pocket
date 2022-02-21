ed941d0aaca057fc4dc0dd3b0411ea578754fd2c
Cameron Hardy <camhardy99@gmail.com>
2022-02-21 16:19:17 -0600

    Refactored to rely on parent state. Removed unused hooks. Added to dependancy array.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,14 @@
-import {useNavigate} from 'react-router-dom';
 import {useState, useEffect} from 'react';
 import BudgetVisual from './BudgetVisual';
 
 function Home(props) {
-    const navigate = useNavigate();
-    const [setBud, setSetBud] = useState(props.setBud);
-    const [currBud, setCurrBud] = useState(props.currBud);
     const [calc, setCalc] = useState(0)
 
     useEffect(() => {
         setTimeout(() => {
-            setCalc((currBud / setBud) * 100)
+            setCalc((props.currBud / props.setBud) * 100)
         }, 250)
-    }, [])
+    }, [props.currBud, props.setBud])
 
     return (
     <>
