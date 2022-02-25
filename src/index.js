541d8e94b162a246b1204f60cea88a790e43ea54
Cameron Hardy <camhardy99@gmail.com>
2022-02-25 11:04:52 -0600

    Revert "Added SuperTokens auth for frontend"
    This reverts commit b015f51499a469549283a5b2335b12cf5a5c20ba.

--- src/index.js
+++ src/index.js
@@ -5,33 +5,12 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Routes, Route} from 'react-router-dom';
 import App from './App';
-import SuperTokens, { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
-import Passwordless from "supertokens-auth-react/recipe/passwordless";
-import Session from "supertokens-auth-react/recipe/session";
-
-SuperTokens.init({
-    appInfo: {
-        appName: "Checkbook-Pocket",
-        apiDomain: "https://family-budget-app-server.herokuapp.com",
-        websiteDomain: "https://checkbook-pocket.netlify.app",
-        apiBasePath: "/auth",
-        websiteBasePath: "/"
-    },
-    recipeList: [
-        Passwordless.init({
-            contactMethod: "PHONE"
-        }),
-        Session.init()
-    ]
-});
-
-
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
-                {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+                <Route path="/" element={<h1>No User</h1>} />
                 <Route path=":id/*" element={<App />} />
                 <Route path="/404" element={<h1>404: Page not found.</h1>} />
             </Routes>
