b015f51499a469549283a5b2335b12cf5a5c20ba
Cameron Hardy <camhardy99@gmail.com>
2022-02-25 10:30:15 -0600

    Added SuperTokens auth for frontend

--- src/index.js
+++ src/index.js
@@ -5,12 +5,33 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Routes, Route} from 'react-router-dom';
 import App from './App';
+import SuperTokens, { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
+import Passwordless from "supertokens-auth-react/recipe/passwordless";
+import Session from "supertokens-auth-react/recipe/session";
+
+SuperTokens.init({
+    appInfo: {
+        appName: "Checkbook-Pocket",
+        apiDomain: "https://family-budget-app-server.herokuapp.com",
+        websiteDomain: "https://checkbook-pocket.netlify.app",
+        apiBasePath: "/auth",
+        websiteBasePath: "/"
+    },
+    recipeList: [
+        Passwordless.init({
+            contactMethod: "PHONE"
+        }),
+        Session.init()
+    ]
+});
+
+
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<h1>No User</h1>} />
+                {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
                 <Route path=":id/*" element={<App />} />
                 <Route path="/404" element={<h1>404: Page not found.</h1>} />
             </Routes>
