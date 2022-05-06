986d5df2b64ee4c2e80bff84ec6212f95516780a
Cameron Hardy <camhardy99@gmail.com>
2022-05-06 15:17:54 -0500

    Added Login component for home screen

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Routes, Route} from 'react-router-dom';
 import App from './App';
+import Login from './components/Login';
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<h1>No User</h1>} />
+                <Route path="/" element={<Login />} />
                 <Route path=":id/*" element={<App />} />
                 <Route path="/404" element={<h1>404: Page not found.</h1>} />
             </Routes>
