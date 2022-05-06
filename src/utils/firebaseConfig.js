8eea8703aa7438529787d455e1f06b8b22496dcb
Cameron Hardy <camhardy99@gmail.com>
2022-05-06 15:20:34 -0500

    Added firebase configuration

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAhcG_p2HDngtPIgd_5i88VDmOW4UrLrDo",
+    authDomain: "checkbook-pocket.firebaseapp.com",
+    projectId: "checkbook-pocket",
+    storageBucket: "checkbook-pocket.appspot.com",
+    messagingSenderId: "641727064942",
+    appId: "1:641727064942:web:43514d900db9835debef7f",
+    measurementId: "G-FZJS2J6KD0"
+};
+
+export default firebaseConfig;
