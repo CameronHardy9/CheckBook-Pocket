ef22b94eaa51e507c435d9404eff2238993e2b57
Cameron Hardy <camhardy99@gmail.com>
2022-05-10 10:18:34 -0500

    added new user endpoint

--- src/utils/apiHandler.js
+++ src/utils/apiHandler.js
@@ -2,6 +2,9 @@ async function apiHandler (method, body) {
     let string = "";
 
     // TODO: Add route for generating new user profile
+    if(method === "POST" && body.id) {
+        string = "";
+    }
 
     if(method === "PUT" && body.store && body.amount && body.date) {
         string = `add/${body.store}/${body.amount}/${body.date}`;
@@ -16,7 +19,8 @@ async function apiHandler (method, body) {
     }
 
     try {
-        let response = await fetch(`https://family-budget-app-server.herokuapp.com/api/${body.id}/${string}`, {
+        // Live endpoint - https://family-budget-app-server.herokuapp.com/api/
+        let response = await fetch(`http://localhost:8080/api/${body.id}/${string}`, {
             mode: 'cors',
             headers: {
                 "Content-Type": "application/json"
